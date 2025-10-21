953f6ddceef628e2ee52bf5e56cf6e15babce65f
roygro <princess18gro@gmail.com>
2025-10-21 17:25:14 -0600

    pagos details y list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,25 @@ import { Recepcionista } from './component/recepcionista/recepcionista';
 import { MembresiaList } from './component/membresias/membresia-list/membresia-list';
 import { MembresiaForm } from './component/membresias/membresia-form/membresia-form';
 import { InstructorComponent } from './component/instructor/instructor-component/instructor-component';
+import { PagoList } from './component/pagos/pago-list/pago-list';
+import { PagoDetails } from './component/pagos/pago-details/pago-details';
+import { PagoCreate } from './component/pagos/pago-create/pago-create';
 
 export const routes: Routes = [
+
+  // Rutas de Pagos
+  { 
+    path: 'pagos', 
+    component: PagoList 
+  },
+  { 
+    path: 'pagos/crear', 
+    component: PagoCreate 
+  },
+  { 
+    path: 'pagos/:id', 
+    component: PagoDetails 
+  },
 
   // Rutas de Membresías
   { 
@@ -19,30 +36,35 @@ export const routes: Routes = [
     path: 'membresias/editar/:id', 
     component: MembresiaForm 
   },
-  
 
   // Ruta para actividades PRIMERO - usando loadComponent
   {
     path: 'actividades', 
     loadComponent: () => import('./component/instructor/actividades-component/actividades-component').then(m => m.ActividadesComponent)
   },
+  
   // Ruta para recepcionista
   {
     path: 'recepcionista', 
     loadComponent: () => import('./component/recepcionista/recepcionista').then(m => m.Recepcionista)
   },
-// Ruta para instructor
-  { path: 'instructores', component: InstructorComponent },
+  
+  // Ruta para instructor
+  { 
+    path: 'instructores', 
+    component: InstructorComponent 
+  },
+  
   // Ruta por defecto
   { 
     path: '', 
     redirectTo: 'recepcionista', 
     pathMatch: 'full' 
   },
+  
   // Ruta comodín
   {
     path: '**',
     redirectTo: 'recepcionista'
-
   }
 ];
---
+++ src/app/component/pagos/pago-create/pago-create.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pago-create',
+  imports: [],
+  templateUrl: './pago-create.html',
+  styleUrl: './pago-create.css'
+})
+export class PagoCreate {
+
+}
